b696ae139c9c989a4bc23d04363d20a6af999753
Jasminder <sjasminder94@gmail.com>
2025-12-27 14:52:30 +0800

    Updated the Model Conversion

--- Jetson_Model.py
+++ Jetson_Model.py
@@ -1,21 +1,10 @@
 from ultralytics import YOLO
-import torch
 
-# Clear memory
-torch.cuda.empty_cache()
+# Load a YOLO11n PyTorch model
+model = YOLO("./Jetson_yolov11n-kitti-Cam-only-7/train/weights/best.pt")
 
-# Load the .pt file
-model = YOLO('Jetson_yolov11n-kitti/train2/weights/best.pt')
-
-# Export ON JETSON
-model.export(
-    format='engine',
-    half=True,
-    imgsz=640,  
-    simplify=True,
-    device=0,   
-    workspace=2 
-)
+# Export the model to TensorRT
+model.export(format="engine", device="0")  # creates 'yolo11n.engine'
 
 print("TensorRT engine created on Jetson!")
 print("TensorRT engine created: best.engine")
